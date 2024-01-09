57e64870be3e68c5589c173bacc2fe0821d97ce5
llihwerd <100llihwerd@gmail.com>
2024-01-08 19:44:41 -0500

    fix redirect, debug package.json

--- server.mjs
+++ server.mjs
@@ -25,6 +25,10 @@ app.set('view engine', 'ejs')
 // Mount routes
 
 app.get('/', function(req, res) {
+  res.redirect('/home')
+})
+
+app.get('/', function(req, res) {
   res.send('<h1>Hello Express</h1>')
 })
 
@@ -38,12 +42,8 @@ app.get('/pokemon', function(req, res) {
   })
 })
 
-app.get('/', function(req, res) {
-  res.redirect('/home')
-})
 
 // tell the app to listen on port 3000
-
 app.listen(3000, function() {
   console.log('Listening on port 3000')
 })
